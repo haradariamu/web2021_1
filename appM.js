c1f6af2de31dcc383553cae260d1eea680ae2393
哩亜夢 <116865013+haradariamu@users.noreply.github.com>
2022-12-29 08:40:18 +0000

    ボタンの追加

--- appM.js
+++ appM.js
@@ -6,6 +6,8 @@ const db = new sqlite3.Database('test4.db');
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const message = "このページはメギド72のキャラに関する情報とボスの攻略に関するページです。";
@@ -56,16 +58,22 @@ app.get("/M-syousai/:id", (req, res) => {
 })
 
 
-app.get("/-syousai", (req, res) => {
-    db.serialize( () => {
-        db.all("CREATE TABLE IF NOT EXISTS MW AS select megido.id, megido.名前,megido.HP,megido.攻撃力,megido.防御力,megido.素早さ,megido.スタイル,megido.クラス,waza.megidoID ,waza.種類,waza.効果 as waza from megido,MW inner join waza on ( (megido.id=MW.megido_id) and (waza.id=MW.waza_id) );", (error, row) => {
-            if( error ) {
-                res.render('toppageM', {mes:"エラーです"});
-            }
-            res.render('M-syousai', {data:row});
-        })
-    })
-})
+app.post("/insert", (req, res) => {
+let sql = `
+insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);
+`
+console.log(sql);
+db.serialize( () => {
+db.run( sql, (error, row) => {
+console.log(error);
+if(error) {
+res.render('show', {mes:"エラーです"});
+}
+res.redirect('/db');
+});
+});
+console.log(req.body);
+});
 
 app.get("/public1/megido.html", (req, res) => {
     db.serialize( () => {"CREATE VIEW MSK AS select * from megido,KS WHERE megido.スタイル = KS.id ,megido.クラス = KS.id ;"
