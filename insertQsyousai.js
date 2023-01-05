1a7191a07095e1bbe2309fa858f01bc8fb23438b
哩亜夢 <116865013+haradariamu@users.noreply.github.com>
2023-01-05 06:37:59 +0000

    修正

--- insertQsyousai.js
+++ insertQsyousai.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test4.db');
 
 let sql = `
-insert into Qsyousai ("QID","Q",詳細) values (4,"呪い","チャージフォトンを使用不可になる");
+insert into Qsyousai ("QID","Q",詳細) values (5,"覚醒ゲージ","ゲージが最大まで溜まっているとスキルが覚醒スキルにアタックが奥義に変化する");
 `
 
 db.serialize( () => {
--- insertyougo.js
+++ insertyougo.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test4.db');
 
 let sql = `
-insert into QA ("Q","A") values ("状態異常","現在１９種類の状態異常が実装されている");
+insert into QA ("Q","A") values ("基本","このゲームを楽しむ上で知っとくべき基本知識");
 `
 
 db.serialize( () => {
